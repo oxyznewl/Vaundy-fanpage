24ce94dbc25b1de8e89e2ae7ac20a146a18d0d74
oxyznewl <limemint668@gmail.com>
2026-06-06 23:19:17 +0900

    공지사항 수정

--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 
-const NOTICES = [
+const NOTICES: { id: number; title: string; date: string }[] = [
   // 나중에 여기에 공지 추가해요
   // { id: 1, title: '공지 제목', date: '2026.06.06' },
 ]
